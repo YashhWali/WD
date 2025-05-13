0a1a44fdb148dc2d193079c52e89d9f3a668c5b1
YashhWali <waliyash879@gmail.com>
2025-05-14 00:36:55 +0530

    Add files via upload

---
+++ reminder.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+ using namespace std;
+int main(){
+int a;
+cout<<"enter the number to find reminder";
+cin>>a;
+if(a<0)
+{
+cout<<"enter +ve value";
+}else{
+cout<<a%2;
+}
+
+}
